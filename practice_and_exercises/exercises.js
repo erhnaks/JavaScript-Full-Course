2d426acb5b12fb48a1a50dd4ec8f3090cf5c4607
erhnaks <erhnaks@hotmail.com>
2022-07-30 09:34:53 +0100

    People counter

---
+++ practice_and_exercises/exercises.js
@@ -0,0 +1,31 @@
+// let count = 0;
+// console.log(count);
+
+// let myAge = 30; //printing the age to the console/
+// console.log(myAge);
+
+//Converting human age to the dog age.
+
+// let humanAge = 30;
+// let humanDogRatio = 7;
+// let myDogAge = humanAge * humanDogRatio;
+
+// console.log(myDogAge);
+
+// adding values to itself
+
+// let bonusPoints = 50;
+
+// console.log(bonusPoints); //50;
+
+// bonusPoints = bonusPoints + 50;
+
+// console.log(bonusPoints); //100;
+
+// bonusPoints = bonusPoints - 75;
+
+// console.log(bonusPoints); //25;
+
+// bonusPoints = bonusPoints + 45;
+
+// console.log(bonusPoints); //70;

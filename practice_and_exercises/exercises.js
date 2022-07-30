107c0ee842f27a4fd6b77afc89bf2513a8ffc484
erhnaks <erhnaks@hotmail.com>
2022-07-30 10:49:59 +0100

    let exercise outside of the scope

--- practice_and_exercises/exercises.js
+++ practice_and_exercises/exercises.js
@@ -32,16 +32,38 @@
 
 //Using functions
 
-function countDown() {
-  console.log(5);
-  console.log(4);
-  console.log(3);
-  console.log(2);
-  console.log(1);
+// function countDown() {
+//   console.log(5);
+//   console.log(4);
+//   console.log(3);
+//   console.log(2);
+//   console.log(1);
+// }
+
+// countDown();
+// console.log("GO GO! GO!");
+// console.log("Ready for the second race?");
+// countDown();
+// console.log("Go! Go! Go! Go! Go! Go! Go! Go! Go! Go!");
+
+// function myLogger() {
+//   console.log(42);
+// }
+
+// myLogger();
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+function logLapTime() {
+  
+  // lapTime = lap1 + lap2 + lap3;
+
+  // console.log(lapTime);
+
+  // Another way of writing by getting the laps out side of the scope of the function.
+
+  console.log(lap1 + lap2 + lap3);
 }
 
-countDown();
-console.log("GO GO! GO!");
-console.log("Ready for the second race?");
-countDown();
-console.log("Go! Go! Go! Go! Go! Go! Go! Go! Go! Go!");
+logLapTime();

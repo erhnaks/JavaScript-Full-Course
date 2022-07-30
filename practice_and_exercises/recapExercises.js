c28762181a23552b24910d3da5b095c712204d5d
erhnaks <erhnaks@hotmail.com>
2022-07-30 14:49:51 +0100

    Solving a challange by writing functions

--- practice_and_exercises/recapExercises.js
+++ practice_and_exercises/recapExercises.js
@@ -5,11 +5,32 @@
 
 // console.log(fullName);
 
-let herName = "Linda";
+// let herName = "Linda";
 
-let greeting = "Hi there";
+// let greeting = "Hi there";
 
-function greetLinda() {
-  console.log(greeting + "," + " " + herName + "!");
+// function greetLinda() {
+//   console.log(greeting + "," + " " + herName + "!");
+// }
+// greetLinda();
+
+let myPoint = 3;
+
+function add3Point() {
+  myPoint += 3;
+  console.log(myPoint);
 }
-greetLinda();
+
+function remove1Point() {
+  myPoint += -1;
+  console.log(myPoint);
+}
+add3Point(); //6
+
+remove1Point(); //5
+
+add3Point(); //8
+
+remove1Point(); //7
+
+add3Point(); //10

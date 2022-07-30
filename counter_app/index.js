2cc74378cbbae93e3340c2ab1beeb27fb7cf76a7
erhnaks <erhnaks@hotmail.com>
2022-07-30 11:45:23 +0100

    save button - save fucntion has been added

--- counter_app/index.js
+++ counter_app/index.js
@@ -9,3 +9,14 @@ function increment() {
   countEl.innerHTML = count;
   console.log(count);
 }
+
+let numberOfPeople = document.getElementById("count-save");
+
+console.log(numberOfPeople);
+
+function savePeople() {
+  numberOfPeople.innerHTML = count;
+
+  savePeople.innerHTML = count;
+  console.log(numberOfPeople);
+}

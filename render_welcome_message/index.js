eb67defe5156ec5c17a40b2fbb8992fe26775e36
erhnaks <erhnaks@hotmail.com>
2022-07-30 12:51:54 +0100

    Welcome greeting in javascript including an emoji

---
+++ render_welcome_message/index.js
@@ -0,0 +1,10 @@
+let welcomeEl = document.getElementById("welcome-el");
+
+let myName = "Javascript Developer";
+
+let greeting = "Hello, ";
+
+welcomeEl.innerText = greeting + myName;
+
+
+welcomeEl.innerText += " 👋 ";

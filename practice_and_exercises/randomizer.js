57dee3fc0316a2d2ad9e8249d5a3d89250f091f5
erhnaks <erhnaks@hotmail.com>
2022-07-31 19:02:10 +0100

    Math.floor and Math.random exercises

---
+++ practice_and_exercises/randomizer.js
@@ -0,0 +1,7 @@
+let randomNumber = Math.floor(Math.random() * 6);
+
+console.log(randomNumber);
+
+// let flooredNumber = Math.floor(3.45632); will get rid of the decimal point
+
+// console.log(flooredNumber);

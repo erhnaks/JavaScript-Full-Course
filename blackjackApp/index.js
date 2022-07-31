93bd6862a63d96373358c97cca73a4d03e6af91c
erhnaks <erhnaks@hotmail.com>
2022-07-31 11:39:31 +0100

    Booleans in if else statements

--- blackjackApp/index.js
+++ blackjackApp/index.js
@@ -1,15 +1,20 @@
-let firstCard = 11;
-
-let secondCard = 11;
-
-let sum = firstCard + secondCard;
+let firstCard = 10;
+let secondCard = 7;
+let sum = firstCard + secondCard + 2;
+let hasBlackJack = false;
+let isAlive = true;
 
 console.log(sum);
 
-if (sum === 21) {
-  console.log("Youuuu are a WINNER!!!");
-} else if (sum < 21) {
+if (sum <= 20) {
   console.log("Do you want to draw an another card?");
+} else if (sum === 21) {
+  console.log("Youuuu are a WINNER!!!");
+  hasBlackJack = true;
 } else {
   console.log("You are busted!!!");
+  isAlive = false;
 }
+
+//console.log(hasBlackJack);
+console.log(isAlive);

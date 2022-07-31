530f6bd91359aa278374c7885a5de850259a86e0
erhnaks <erhnaks@hotmail.com>
2022-07-31 11:09:35 +0100

    more exercises if else!

--- practice_and_exercises/exercises.js
+++ practice_and_exercises/exercises.js
@@ -83,10 +83,20 @@
 
 // If else statements exercise;
 
-let age = 22;
+// let age = 22;
 
-if (age >= 21) {
-  console.log("Welcome to the club");
+// if (age >= 21) {
+//   console.log("Welcome to the club");
+// } else {
+//   console.log("You can not enter the club!");
+// }
+
+let age = 100
+
+if (age < 100) {
+  console.log("You are not eligible");
+} else if (age === 100) {
+  console.log("Here is your birthday card from the King!");
 } else {
-  console.log("You can not enter the club!");
+  console.log("Not eligible you've already got one!");
 }

967de93805a531c2152c49db2866c37894f64a9e
erhnaks <erhnaks@hotmail.com>
2022-07-31 10:14:39 +0100

    else - if statement modified

--- blackjackApp/index.js
+++ blackjackApp/index.js
@@ -1,7 +1,15 @@
-let firstCard = 6;
+let firstCard = 11;
 
-let secondCard = 8;
+let secondCard = 11;
 
 let sum = firstCard + secondCard;
 
 console.log(sum);
+
+if (sum === 21) {
+  console.log("Youuuu are a WINNER!!!");
+} else if (sum < 21) {
+  console.log("Do you want to draw an another card?");
+} else {
+  console.log("You are busted!!!");
+}
